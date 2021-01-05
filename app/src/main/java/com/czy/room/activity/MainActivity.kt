874a7c990e5aa03a39031f8026b2d96ac5d85e38
zhaoyang.cheng <xiayilz@126.com>
2021-01-05 17:11:06 +0800

    up

--- app/src/main/java/com/czy/room/activity/MainActivity.kt
+++ app/src/main/java/com/czy/room/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.czy.room.activity
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -7,10 +8,12 @@ import com.czy.room.R
 import com.czy.room.db.AppDataBase
 import com.czy.room.db.entity.User
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,21 +22,29 @@ class MainActivity : AppCompatActivity() {
                 val list = ArrayList<String>()
                 list.add("1111")
                 list.add("2222")
-                AppDataBase.getInstance(this).getUserDao().insert(User(name = "张三",age = 22,likes = list))
+                AppDataBase.getInstance(this).getUserDao()
+                    .insert(User(name = "张三" + System.currentTimeMillis(), age = 22, likes = list))
             }
         }
 
         btn_query.setOnClickListener {
             thread {
                 val list = AppDataBase.getInstance(this).getUserDao().queryAll()
-                for (it in list){
-                    log("_id=${it._id},name=${it.name},age=${it.age}")
+                val stringBuilder = StringBuilder()
+                for (it in list) {
+                    stringBuilder.append("_id=${it._id},name=${it.name},age=${it.age}\n")
                 }
+
+                runOnUiThread {
+                    tv_result.text = stringBuilder
+                }
+
             }
         }
 
     }
 }
-inline fun log(msg:Any){
-    Log.d("DeBug",msg.toString())
+
+inline fun log(msg: Any) {
+    Log.d("DeBug", msg.toString())
 }
